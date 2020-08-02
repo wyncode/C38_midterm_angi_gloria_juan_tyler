a3dbc9179a038675d2fc8ec2033f295421932191
amgarz0n <angigarzon@gmail.com>
2020-08-02 13:48:16 -0400

    first push

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import { AppContextProvider } from './context/AppContext';
-import ContextDemo from './components/ContextDemo';
-
+import React from 'react'
+// import React, { useState, useEffect } from 'react';
+// import { AppContextProvider } from './context/AppContext';
+// import ContextDemo from './components/ContextDemo';
+import Footer from "./components/Footer";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
 
 const App = () => {
-  const [serverMessage, setServerMessage] = useState('');
+  // const [serverMessage, setServerMessage] = useState('');
 
-  const fetchDemoData = () => {
-    fetch('/api/demo')
-      .then((response) => response.json())
-      .then((data) => setServerMessage(data.message));
-  };
+  // const fetchDemoData = () => {
+  //   fetch('/api/demo')
+  //     .then((response) => response.json())
+  //     .then((data) => setServerMessage(data.message));
+  // };
 
-  useEffect(fetchDemoData, []);
+  // useEffect(fetchDemoData, []);
 
   return (
-    <AppContextProvider>
-      <div id="demo">
-        <h3>Hello from client/src/App.js</h3>
-        <ContextDemo />
-        <h3>{serverMessage}</h3>
-      </div>
-    </AppContextProvider>
+        <Footer />
+
+    // <AppContextProvider>
+    //   <div id="demo">
+    //     <h3>Hello from client/src/App.js</h3>
+    //     <ContextDemo />
+    //     <h3>{serverMessage}</h3>
+    //   </div>
+    // </AppContextProvider>
   );
 };
 
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import '../App.css';
+
+
+const Footer = () => {
+    return (
+<footer className="footer">
+            <h3>JOIN OUR MAILING LIST</h3>
+            <p>Need exciting copy to put here, so people will subscribe if they feel inclined to!</p>
+        <Form className="footer-form">
+
+            <div className="inputs">
+        <Form.Group controlId="formBasicName" className="input-box" >
+            <Form.Control type="name" placeholder="Enter your Name" />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicEmail" className="input-box" >
+            <Form.Control type="email" placeholder="Enter your Email" />
+        </Form.Group>
+        </div>
+        <div className="submit-agreement">
+        <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Sign me up" />
+            <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+            </Form.Text>
+        </Form.Group>
+        
+        <Button variant="primary" type="submit" className="submit_button">
+            Submit
+        </Button>
+        </div>
+        <div className="social-media-icons">
+            <ul>
+                <li><a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook-square"></i></a></li>
+                <li><a href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram-square"></i></a></li>
+                <li><a href="https://twitter.com/" target="_blank"><i class="fab fa-twitter-square"></i></a></li>
+            </ul>
+        </div>
+        </Form>
+</footer>
+    )
+}
+
+export default Footer;
