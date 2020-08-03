f1f5ff5b018a402fdc0b34c5295689588f6e2d73
Gloria Reyes <gloriareyes@Glorias-MacBook-Air.local>
2020-08-03 08:48:25 -0400

    fix conflixt

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { AppContextProvider } from './context/AppContext';
-import ContextDemo from './components/ContextDemo';
 import Modal from './components/Modal';
 
 import './App.css';
@@ -8,16 +6,7 @@ import Footer from './components/Footer/Footer.jsx';
 import About from './components/About/About';
 
 const App = () => {
-  const [serverMessage, setServerMessage] = useState('');
   const [isOpen, setIsOpen] = useState(false);
-
-  const fetchDemoData = () => {
-    fetch('/api/demo')
-      .then((response) => response.json())
-      .then((data) => setServerMessage(data.message));
-  };
-
-  useEffect(fetchDemoData, []);
 
   const handleClick = () => {
     setIsOpen(true);
@@ -28,17 +17,12 @@ const App = () => {
   };
 
   return (
-    <AppContextProvider>
-      <div id="demo">
-        <h3>Hello from client/src/App.js</h3>
-        <ContextDemo />
-        <h3>{serverMessage}</h3>
-      </div>
+    <>
       <button onClick={handleClick}>Show modal</button>
       <Modal isOpen={isOpen} closeModal={closeModal}>
         <h1>this is the stuff inside the modal</h1>
       </Modal>
-    </AppContextProvider>
+    </>
   );
 };
 
