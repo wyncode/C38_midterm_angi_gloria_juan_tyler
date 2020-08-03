f5bec3476666d380f5235d41c179d00bfbfaf870
amgarz0n <angigarzon@gmail.com>
2020-08-03 14:17:58 -0400

    Linked the Feature.css to the Feature.jsx

--- client/src/components/Feature/Feature.jsx
+++ client/src/components/Feature/Feature.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { Card } from 'react-bootstrap';
-// import './App.css';
+import '../Feature/Feature.css';
 
 const Feature = () => {
   // const [ , ] = useState()
