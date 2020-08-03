6cd9dd34c382f9c87067cede3f575d52e7e686ff
amgarz0n <angigarzon@gmail.com>
2020-08-02 21:34:08 -0400

    pushing with the About page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './components/Home/Home';
 import NavBar from './components/Navbar/navbar';
 import './App.css';
 import Footer from './components/Footer/Footer.jsx';
+import About from './components/About/About';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <NavBar />
       <Route path="/home" exact component={Home} />
       <Route path="/breweries" component={Breweries} />
+      <Route path="/about" component={About} />
       <Footer />
     </Router>
     
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -7,9 +7,10 @@ const Home = () => {
       <div className="banner">
         <h5>Welcome to the Brewery Finder!</h5>
         <h1>Immerse yourself into the refreshing lifestyle of brewery touring</h1>
-        <p className="button-placeholder">Component with Button goes here!!</p>
+        <p className="button-placeholder">Find Breweries Now</p>
       </div>
       <div className="container">
+        {/* <img src="client/public/logo-color.png" alt="Brewery Finder Logo"></img> */}
         <h1>Our Story</h1>
         <p>The miller light beyond an Ellis Island IPA throws some Amarillo Pale Ale at an Imperial Stout around an air hocky table, or a change for a bar tab trades baseball cards with the Amarillo Pale Ale inside a beer.
           The mitochondrial monkey bite finds much coolness with a Dixie Beer defined by some bud light.</p>
