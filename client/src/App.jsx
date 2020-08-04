54b4c994d19c0982c903b54566ebae93d928978d
Gloria Reyes <gloriareyes@Glorias-MacBook-Air.local>
2020-08-03 22:45:47 -0400

    Gloria Changing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,71 @@
 import React, { useState, useEffect } from 'react';
-import Modal from './components/Modal';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import Modal from './components/Modal';
 import './App.css';
 import Footer from './components/Footer/Footer.jsx';
 import About from './components/About/About';
+import Feature from './components/Feature/Feature';
+import NavBar from './components/Navbar/navbar';
+import Home from './components/Home/Home';
+import Breweries from './components/Brewery/Breweries';
 
 const App = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleClick = () => {
-    setIsOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsOpen(false);
-  };
-
   return (
-    <>
-      <button onClick={handleClick}>Show modal</button>
-      <Modal isOpen={isOpen} closeModal={closeModal}>
-        <h1>this is the stuff inside the modal</h1>
-      </Modal>
-    </>
+    <Router>
+      <NavBar />
+      <Route path="/home" exact component={Home} />
+      <Route path="/breweries" component={Breweries} />
+      <Route path="/about" component={About} />
+      <Route path="/feature" component={Feature} />
+      <Footer />
+    </Router>
   );
 };
 
 export default App;
+
+// import React, { useState, useEffect } from 'react';
+// import { AppContextProvider } from './context/AppContext';
+// import ContextDemo from './components/ContextDemo';
+// import Modal from './components/Modal';
+
+// import './App.css';
+// import Footer from './components/Footer/Footer.jsx';
+// import About from './components/About/About';
+
+// const App = () => {
+//   const [serverMessage, setServerMessage] = useState('');
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const fetchDemoData = () => {
+//     fetch('/api/demo')
+//       .then((response) => response.json())
+//       .then((data) => setServerMessage(data.message));
+//   };
+
+//   useEffect(fetchDemoData, []);
+
+//   const handleClick = () => {
+//     setIsOpen(true);
+//   };
+
+//   const closeModal = () => {
+//     setIsOpen(false);
+//   };
+
+//   return (
+//     <AppContextProvider>
+//       <div id="demo">
+//         <h3>Hello from client/src/App.js</h3>
+//         <ContextDemo />
+//         <h3>{serverMessage}</h3>
+//       </div>
+//       <button onClick={handleClick}>Show modal</button>
+//       <Modal isOpen={isOpen} closeModal={closeModal}>
+//         <h1>this is the stuff inside the modal</h1>
+//       </Modal>
+//     </AppContextProvider>
+//   );
+// };
+
+// export default App;
---
+++ client/src/components/Feature/Feature.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
+import '../Feature/Feature.css';
+
+const Feature = () => {
+  const [email, setEmail] = useState('');
+
+  const subscribed = () => {
+    const emailValid = email.search('@');
+    if (emailValid !== -1) {
+      alert('Thanks for subscribing!');
+    }
+  };
+
+  return (
+    <div className="container d-flex flex-column justify-content-center align-items-center">
+      <Card style={{ width: '45rem' }}>
+        <Card.Img
+          variant="top"
+          src="https://thefullpint.com/wp-content/uploads/J-Wakefield-Taproom.jpg"
+        />
+        <Card.Body id="cardbody">
+          <Card.Title>Talk of the Town</Card.Title>
+          <Card.Text>
+            <h3>Wynwood Brewery</h3> Nothing quenches the thirst for fun in the
+            artsy town of Wynwood like its pride and joy distillery, Wynwood
+            Brewery
+          </Card.Text>
+
+          <Button
+            id="websitebutton"
+            variant="primary"
+            href="https://wynwoodbrewing.com/"
+            target="_blank"
+          >
+            Website
+          </Button>
+        </Card.Body>
+      </Card>
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Subscribe!</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <Form.Text className="text-muted">
+            Just for you! Receive updates on this awesome brewery's finest!
+          </Form.Text>
+        </Form.Group>
+        <Button onClick={subscribed} variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default Feature;
