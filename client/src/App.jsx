05978a67e7cb2bb704781a9e7a1aa5d79034872b
amgarz0n <angigarzon@gmail.com>
2020-08-05 10:45:27 -0400

    deletefixed the app.jsx, deleted commented code, linked the css to the modal, changed modal styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, {useState} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import Footer from './components/Footer/Footer.jsx';
 import About from './components/About/About';
 import Feature from './components/Feature/Feature';
-import NavBar from './components/Navbar/navbar';
+import NavBar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Breweries from './components/Brewery/Breweries';
 import Maps from './components/Maps/Maps';
@@ -12,46 +12,46 @@ import './App.css';
 import AgeCheck from './components/Modal/Modal';
 
 const App = () => {
-  const [brewery, setBrewery] = useState([]);
-  const [city, setCity] = useState('');
-  const [name, setName] = useState('');
-  console.log('parents', brewery);
-  return (
-    <Router>
-      <NavBar />
+    const [brewery, setBrewery] = useState([]);
+    const [city, setCity] = useState('');
+    const [name, setName] = useState('');
+    console.log('parents', brewery);
+    return (
+        <Router>
+            <NavBar/>
 
-      <AgeCheck />
+            <AgeCheck/>
 
-      <Route path="/" exact component={Home} />
-      <Route
-        exact
-        path="/breweries"
-        render={(props) => {
-          return (
-            <Breweries
-              {...props}
-              brewery={brewery}
-              city={city}
-              name={name}
-              setBrewery={setBrewery}
-              setCity={setCity}
-              setName={setName}
-            />
-          );
-        }}
-      />
-      <Route
-        exact
-        path="/maps/:id"
-        render={(props) => {
-          return <Maps {...props} brewery={brewery} />;
-        }}
-      />
-      <Route path="/about" component={About} />
-      <Route path="/feature" component={Feature} />
-      <Footer />
-    </Router>
-  );
+            <Route path="/" exact
+                component={Home}/>
+            <Route exact path="/breweries"
+                render={
+                    (props) => {
+                        return (
+                            <Breweries {...props}
+                                brewery={brewery}
+                                city={city}
+                                name={name}
+                                setBrewery={setBrewery}
+                                setCity={setCity}
+                                setName={setName}/>
+                        );
+                    }
+                }/>
+            <Route exact path="/maps/:id"
+                render={
+                    (props) => {
+                        return <Maps {...props}
+                            brewery={brewery}/>;
+                    }
+                }/>
+            <Route path="/about"
+                component={About}/>
+            <Route path="/feature"
+                component={Feature}/>
+            <Footer/>
+        </Router>
+    );
 };
 
 export default App;
