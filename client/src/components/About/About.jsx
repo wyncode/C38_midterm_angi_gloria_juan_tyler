b1164da99d47d923d9da87efa32028016f43ecfd
Gloria Reyes <gloriareyes@Glorias-MacBook-Air.local>
2020-08-05 09:54:32 -0400

    Gloria About US change Wed

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -5,6 +5,7 @@ import '../About/About.css';
 import angi from './ImagesPhotos/ANGI.jpg';
 import juan from './ImagesPhotos/Juan.jpg';
 import gloria from './ImagesPhotos/gloriaBeer.jpg';
+import ty from './ImagesPhotos/image0.jpeg';
 
 const About = () => {
   //   const TeamMember = (props) => {
@@ -24,28 +25,30 @@ const About = () => {
       <p>
         Industrious and in the pursuit of transformative tech, the Team knows
         very well how important taking breaks is to achieving success! Here are
-        the members who bring you
+        the members who bring you the exhilerating component of Brewery Finder!
       </p>
+
       <div className="Allteamcards">
         <TeamMember
           name="Angi 'Swift Phalanges'"
-          body="Enjoys long coding days with Corona and lime in hand"
+          body="Enjoys long coding days with Lulo Sour in hand."
           img={angi}
         />
+
         <TeamMember
           name="Juan the Nodemon"
-          body="Loves a tall glass of La Rubia with his code"
+          body="Prefers blondes, so La Rubia is by his coding side."
           img={juan}
         />
         <TeamMember
           name="Gloria Tells a Storia"
-          body="Likes to be not only a 'Modelo' drinker, but coder"
+          body="Likes to be not only a 'Modelo' drinker, but modelo coder too."
           img={gloria}
         />
         <TeamMember
-          name="My name 2"
-          body="Description of person 2 here"
-          img="Hello1"
+          name="Ty the Full Stack Guy"
+          body="Heine-win! Takes big sips of the latest and greatest in the tech world."
+          img={ty}
         />
       </div>
     </main>
